674fa504a609e4750728578a40dae40d25cd87fc
KI MIN SOO <banna111222@gmail.com>
2020-11-29 01:59:31 +0900

    00 등 예외사항은 어떻게 처리해야할까?

--- app/src/main/java/com/example/collatzcalculator/MainActivity.java
+++ app/src/main/java/com/example/collatzcalculator/MainActivity.java
@@ -9,10 +9,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
-    int collatNum = 0;
+    long collatNum = 0;
+    int stages = 1;
+
     Button startBtn = null;
+    EditText stageNum = null;
     EditText collatz = null;
+    TextView numOfStages = null;
     TextView result = null;
     String res = "";
     String ziro = "";
@@ -23,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startBtn = (Button) findViewById(R.id.startBtn);
+        stageNum = (EditText) findViewById(R.id.stageNum);
+        numOfStages = (TextView) findViewById(R.id.stage);
         collatz = (EditText) findViewById(R.id.collatzNum);
         result = (TextView) findViewById(R.id.result);
     }
@@ -34,25 +42,36 @@ public class MainActivity extends AppCompatActivity {
                     if(collatz.getText().toString().equals(ziro)){
                         Toast.makeText(getApplicationContext(), "숫자를 입력해주세요!", Toast.LENGTH_LONG).show();
                         result.setText("");
+                        stageNum.setText("0");
+                        break;
+                    }
+                    else if(collatz.getText().toString().equals("0")){
+                        Toast.makeText(getApplicationContext(), "0보다 큰 수를 입력해주세요!", Toast.LENGTH_LONG).show();
+                        result.setText("");
+                        stageNum.setText("0");
                         break;
                     }
                     else {
                         result.setText("");
-                        collatNum = Integer.parseInt(collatz.getText().toString());
+                        collatNum = Long.parseLong(collatz.getText().toString());
                         res += String.valueOf(collatNum);
                         while (collatNum != 1) {
                             if (collatNum % 2 == 1) {
                                 collatNum = collatNum * 3 + 1;
                                 res += " - ";
                                 res += String.valueOf(collatNum);
+                                stages+=1;
                             } else {
                                 collatNum = collatNum / 2;
                                 res += " - ";
                                 res += String.valueOf(collatNum);
+                                stages+=1;
                             }
                         }
                         result.setText(res);
+                        stageNum.setText(String.valueOf(stages));
                         res="";
+                        stages=1;
                         break;
                     }
 
